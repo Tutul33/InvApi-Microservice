e8ea3605f490277a3dd80753acf63b3b165fc081
Tutul Chakma <tutulcou@gmail.com>
2024-07-01 16:47:25 +0600

    Add project files.

---
+++ InvApi/Controllers/ItemController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RabbitMQ.Client;
+using System.Text.Json;
+using System.Text;
+
+namespace InvApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ItemController : ControllerBase
+    {
+        
+    }    
+}
